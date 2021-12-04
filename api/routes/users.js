61e0ad81a44ac9d80893cd1307849f069772c637
lilyobeng <68597243+lilyobeng@users.noreply.github.com>
2021-12-03 22:16:41 -0500

    Delete api directory

--- api/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const usersCtrl = require('../controllers/users')
-
-router.post('/signup', usersCtrl.signup)
-router.post('/login', usersCtrl.login)
-
-module.exports = router;
